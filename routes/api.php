b9314ffa58d3bd82693a5fc121834558abff4306
dsdeloar02 <dsdeloar02@gmail.com>
2022-01-06 17:17:45 +0600

    'auth'

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\CetagoryController;
 use App\Http\Controllers\CurrencyController;
 use App\Http\Controllers\SubcetagoryController;
@@ -21,6 +22,12 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+Route::group(['middleware'=>'api','prefix'=>'auth'],function($router){
+    Route::post('/register', [AuthController::class, 'register']);
+    Route::post('/login', [AuthController::class, 'login']);
+    Route::get('/profile', [AuthController::class, 'profile']);
+    Route::post('/logout', [AuthController::class, 'logout']);
+});
 
 Route::get('/categories', [CetagoryController::class, 'index']);
 Route::post('/category', [CetagoryController::class, 'store']);
